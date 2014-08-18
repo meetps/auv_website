28cd24d78e4fee2eeaa2c1743f2e84f3897003f2
meetshah1995 <meetshah1995@iitb.ac.in>
2014-08-18 22:08:22 +0530

    php files added

---
+++ includes/contact.php
@@ -0,0 +1,24 @@
+ <?php
+       // from the form
+       $name = trim(strip_tags($_POST['contact_name'])); 
+       $email = trim(strip_tags($_POST['contact_email']));
+       $subject = trim(strip_tags(($_POST['contact_subject'])));
+       $message = htmlentities($_POST['contact_message']);
+
+       // set here
+       $to = 'contact@auv-iitb.org';
+
+       $body = <<<HTML
+$message
+HTML;
+
+       $headers = "From: $email\r\n";
+       $headers .= "Content-type: text/html\r\n";
+
+       // send the email
+       mail($to, $subject, $body, $headers);
+
+       // redirect afterwords, if needed
+       header('Location: index.html');
+
+?>       
