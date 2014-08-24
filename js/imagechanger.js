7a1443623da03a4091492adedf90c178b634232e
meetshah1995 <meetshah1995@iitb.ac.in>
2014-08-24 16:22:40 +0530

    Image Changing Script added

---
+++ js/imagechanger.js
@@ -0,0 +1,55 @@
+var imgObj = null;
+
+function m1_changeToe2(x)
+{  	
+imgObj = document.getElementById('m1-elec');
+imgObj.src = "../images/matsya1/elec2.jpg";
+}
+
+function m1_changeTom2(x)
+{   
+imgObj = document.getElementById('m1-mech');
+imgObj.src = "../images/matsya1/mech2.jpg";
+}
+
+function m1_changeTos2(x)
+{   
+imgObj = document.getElementById('m1-soft');
+imgObj.src = "../images/matsya1/soft2.jpg";
+}
+
+function m2_changeToe2(x)
+{   
+imgObj = document.getElementById('m2-elec');
+imgObj.src = "../images/matsya2/elec2.jpg";
+}
+
+function m2_changeTom2(x)
+{   
+imgObj = document.getElementById('m2-mech');
+imgObj.src = "../images/matsya2/elec2.jpg";
+}
+
+function m2_changeTos2(x)
+{   
+imgObj = document.getElementById('m2-soft');
+imgObj.src = "../images/matsya2/elec2.jpg";
+}
+
+function m3_changeToe2(x)
+{   
+imgObj = document.getElementById('m3-elec');
+imgObj.src = "../images/matsya3/elec2.jpg";
+}
+
+function m3_changeTom2(x)
+{   
+imgObj = document.getElementById('m3-mech');
+imgObj.src = "../images/matsya3/elec2.jpg";
+}
+
+function m3_changeTos2(x)
+{   
+imgObj = document.getElementById('m3-soft');
+imgObj.src = "../images/matsya3/elec2.jpg";
+}
